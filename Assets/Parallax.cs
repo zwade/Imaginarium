0bca34b2602f22cd40e732711071401b7853c829
zwade <zacharywade@gmail.com>
2015-03-20 21:24:01 -0400

    lots of parallax

---
+++ Assets/Parallax.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour {
+	public float scale = 2;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition = new Vector3(-transform.parent.transform.parent.transform.localPosition.x/scale,transform.localPosition.y,transform.localPosition.z);
+	}
+}
