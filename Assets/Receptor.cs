9a87b2ccb97620e0f97716ea4b9e0daa83d25ed9
zwade <zacharywade@gmail.com>
2015-03-20 22:37:49 -0400

    first level done

--- Assets/Receptor.cs
+++ Assets/Receptor.cs
@@ -1,16 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Receptor : MonoBehaviour {
 	public float real = -2;
 	public float imag = 0;
+	public GameObject text;
 
 	public Complex location;
 	// Use this for initialization
 	void Start () {
 		location = new Complex(real, imag);
 		transform.position = location.toVector3 + transform.position.y * Vector3.up ;
-
+		text.GetComponent<Text>().text = location.ToString ();
+		transform.eulerAngles = new Vector3(0,location.Phase*-180/Mathf.PI,0);
 	
 	}
 	
